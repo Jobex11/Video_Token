1720fafc983f76450bf2afb6493e056ea7514237
jobex11 <joblawal33@gmail.com>
2025-02-25 16:35:10 +0100

    added walletconect

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,8 +2,11 @@ import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 const Home = () => {
   return (
-    <div>
-      <WalletMultiButton />
+    <div className="w-full flex flex-col items-center justify-center bg-red-50 h-[100vh]">
+      <div className="text-2xl m-4 font-semibold">Video-Token</div>
+      <div>
+        <WalletMultiButton />
+      </div>
     </div>
   );
 };
